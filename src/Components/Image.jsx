fdb61f71b81a22554bc53c4046704bfd07296a35
Rose <rosebeats09@gmail.com>
2024-03-11 18:02:02 -0400

    Fetch all the elements of pokemon array with promise.all()

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -19,10 +19,23 @@ export default function Image() {
     
     // forms -> "front_default":"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
     useEffect(() => {
-        fetch(pokemons[0]).then(function(response) {
-        return response.json();
-    }) 
-    }, );
-    
-    
+        // // asynchronous function to fetch pokemon of the respective url
+        const fetchPokemon = async (url) => {
+            const res = await fetch(url);
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        };
+        // the pokemons.map((item) creates an array of promises by mapping each URL
+        // to the result of the fetchPokemon function. Promise.all then waits for all these promises to settle
+        // (either resolve or reject)
+        Promise.all(pokemons.map((item) => fetchPokemon(item)))
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => console.error(err));
+        
+    }, )
+ 
 }
